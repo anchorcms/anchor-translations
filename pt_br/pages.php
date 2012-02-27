35ea75039633227781cb0be4bf252dd439757b2d
Visual Idiot <iam@visualidiot.com>
2012-02-27 11:54:28 +0000

    Added Brazilian Portugese, thanks to @persocon.

---
+++ pt_br/pages.php
@@ -0,0 +1,41 @@
+<?php defined('IN_CMS') or die('No direct access allowed.');
+
+return array(
+
+	'pages' => 'P&aacute;ginas',
+	'create_page' => 'Criar uma nova p&aacute;gina',
+	'no_pages' => 'Nenhuma p&aacute;gina criada ainda. Por que n&atilde;o escrever uma?',
+	'add_page' => 'Adicionar p&aacute;gina',
+	'editing' => 'Editar',
+	'editing_explain' => 'Alguns links &uacute;teis.',
+	'view_page' => 'Ver esta p&aacute;gina no seu site',
+
+	'name' => 'Nome',
+	'name_explain' => 'O nome da p&aacute;gina. Isto ser&aacute; mostrado na navega&ccedil;&atilde;o.',
+	'title' => 'T&iacute;tulo',
+	'title_explain' => 'O t&iacute;tulo da p&aacute;gina que ser&aacute; mostrato no <code>&lt;title&gt;</code>.',
+	'slug' => 'Link Permanente',
+	'slug_explain' => 'O link permanente do seu post (<code>' . $_SERVER['HTTP_HOST'] . '/<span id="output">link permanente</span></code>).',
+	'content' => 'Conte&uacute;do',
+	'content_explain' => 'O conte&uacute;do da sua p&aacute;gina. HTML v&aacute;lido aceito.',
+	'status' => 'Status',
+	'status_explain' => 'Voc6e quer que sua p&aacute;gina seja live (publicada), pendente (rascunho), ou escondida (arquivada)?',
+
+	'draft' => 'Rascunho',
+	'archived' => 'Arquivada',
+	'published' => 'Publicada',
+
+	'create' => 'Criar',
+	'delete' => 'Remover',
+	'save' => 'Salvar',
+	'return_pages' => 'Retornar as P&aacute;ginas',
+
+	'missing_name' => 'Por favor insira um nome',
+	'missing_title' => 'Por favor insira um t&iacute;tulo',
+	'duplicate_slug' => 'Uma p&aacute;gina com o mesmo link permanente j&aacute; exite, por favor altere o link permanente desta p&aacute;gina.',
+	'page_success_created' => 'Sua nova p&aacute;gina foi adicionada',
+	'page_success_updated' => 'Sua p&aacute;gina foi atualziada',
+	'page_success_delete' => 'Sua p&aacute;gina foi removida',
+	'page_error_delete' => 'Desculpe, voc&ecirc; n&atilde;o pode remover sua p&aacute;gina principal ou sua p&aacute;gina de posts.'
+
+);
