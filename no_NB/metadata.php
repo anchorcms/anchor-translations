2603b4054fb460e37117e7436d98ba6fa66a02c5
Øystein Efterdal <oystein@efterdal.no>
2013-03-07 16:38:55 +0100

    Added Norwegian Translation
    Ca 30% done

---
+++ no_NB/metadata.php
@@ -0,0 +1,29 @@
+<?php defined('IN_CMS') or die('Ingen direkte aksess er tillatt.');
+
+return array(
+
+	'metadata' => 'Websidens metadata',
+
+	'sitename' => 'Sidensnavn',
+	'sitename_explain' => 'Navnet på nettsiden din.',
+	'sitedescription' => 'Websidebeskrivelse',
+	'sitedescription_explain' => 'En kort beskrivelse av din nettside.',
+	'homepage' => 'Startsiden',
+	'homepage_explain' => 'Din aktive startside.',
+	'postspage' => 'Innleggs side',
+	'postspage_explain' => 'Siden som kommer til å vise dine innlegg.',
+	'posts_per_page' => 'Innlegg per side',
+	'posts_per_page_explain' => 'Antall innlegg per som vises per side.',
+	'current_theme' => 'Aktivt tema',
+	'current_theme_explain' => 'Ditt aktive tema.',
+	'auto_publish_comments' => 'Publiser kommentarer automatisk',
+	'twitter' => 'Twitter',
+	'twitter_explain' => 'Din twitter konto. Vises som @<span id="output"></span>.',
+	'save' => 'Lagre endringer',
+
+	'missing_sitename' => 'Du må angi et navn på nettsiden',
+	'missing_sitedescription' => 'Du må angi en beskrivelse av nettsiden',
+	'missing_theme' => 'Du må velge et tema',
+	'meta_success_updated' => 'Din metadata ble oppdatert'
+
+);
