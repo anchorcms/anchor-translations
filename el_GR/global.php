c0b2a98da511391c89d2cd52d13a7fb760db5d87
Gexos <gexosorg@gmail.com>
2013-09-27 17:00:56 +0300

    Added Greek Translations

---
+++ el_GR/global.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Αποθήκευση',
+	'delete' => 'Διαγραφή',
+	'update' => 'Ενημέρωση',
+	'edit' => 'Επεξεργασία',
+	'editing' => 'Επεξεργασία',
+	'create' => 'Δημιουργία',
+	'created' => 'Δημιουργήθηκε',
+	'submit' => 'Υποβολή',
+	'close' => 'Κλείσιμο',
+	'status' => 'Κατάσταση',
+	'manage' => 'Διαχείριση',
+	'reset' => 'Επαναφορά',
+	'all' => 'Όλα',
+
+	// pagination
+	'next' => 'Επόμενο',
+	'previous' => 'Προηγούμενο',
+	'first' => 'Πρώτο',
+	'last' => 'Τελευταίο',
+
+	// statuses
+	'draft' => 'Προσχέδιο',
+	'archived' => 'Αρχειοθετημένο',
+	'published' => 'Δημοσιεύτηκε',
+	'pending' => 'Σε αναμονή',
+	'approved' => 'Εγκρίθηκε',
+	'spam' => 'Spam',
+
+	'inactive' => 'Ανενεργό',
+	'active' => 'Ενεργό',
+
+	// roles
+	'administrator' => 'Διαχειριστής',
+	'editor' => 'Συντάκτης',
+	'user' => 'Χρήστης',
+
+	'log_in' => 'Σύνδεση',
+	'login' => 'Σύνδεση',
+	'log_out' => 'Αποσύνδεση',
+	'logout' => 'Αποσύνδεση',
+
+	// pharses
+	'visit_your_site' => 'Επισκεφθείτε την ιστοσελίδα σας',
+	'powered_by_anchor' => 'Powered by Anchor, έκδοση %s',
+	'make_blogging_beautiful' => 'Make blogging beautiful.',
+
+	// messages
+	'confirm_delete' => 'Είστε σίγουροι ότι επιθυμείτε να διαγράψετε? Αυτή η λειτουργία δεν μπορεί να αναιρεθεί!'
+
+);
---
+++ el_GR/plugins.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'plugins' => 'Πρόσθετα ',
+	'plugins_desc' => 'Διαχείριση Πρόσθετων',
+	'noplugins_desc' => 'Δεν υπάρχουν Πρόσθετα',
+
+	// messages
+	'installed' => '%s έχει εγκατασταθεί',
+	'uninstalled' => '%s έχει απεγκατασταθεί'
+);
