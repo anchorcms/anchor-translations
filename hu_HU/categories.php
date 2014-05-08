0f492ac77c76864427dca40b0db2589e3fa61b24
Laszlo Espadas <kw@kardiweb.org>
2014-05-08 21:18:43 +0200

    Update hungarian translation 0.9.2

--- hu_HU/categories.php
+++ hu_HU/categories.php
@@ -2,11 +2,27 @@
 
 return array(
 
-	'missing_title' => 'Kérlek add meg a címet',
-	'category_failed_delete' => 'Egy kategóriának lennie kell',
+	'category' => 'Kategória',
+	'categories' => 'Kategória',
 
-	'category_success_created' => 'Az új kategóriád hozzáadva.',
-	'category_success_updated' => 'A kategóriád frissitve.',
-	'category_success_deleted' => 'A kategóriád törölve.'
+	'create_category' => 'Új kategória',
+	'edit_category' => '&ldquo;%s&rdquo; szerkesztés',
+
+	// form fields
+	'title' => 'Név',
+	'title_explain' => 'Kategória név',
+	'title_missing' => 'Adjuk meg a kategória nevet',
+
+	'slug' => 'Keresőbarát név',
+	'slug_explain' => 'Kategória keresőbarát név.',
+
+	'description' => 'Leírás',
+	'description_explain' => 'Kategória leírása.',
+
+	// messages
+	'created' => 'Az új kategória hozzáadásra került.',
+	'updated' => 'A kategória frissítésre került.',
+	'deleted' => 'A kategória törlésre került.',
+	'delete_error' => 'Válasszunk ki legalább egy kategóriát.',
 
 );
