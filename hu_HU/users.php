b9d3aff7eb420085cccc33b30fbacb0379c5c573
Laszlo Espadas <kw@kardiweb.org>
2014-05-08 21:21:53 +0200

    Update hungarian translation 0.9.2

--- hu_HU/users.php
+++ hu_HU/users.php
@@ -2,74 +2,66 @@
 
 return array(
 
-	'add_user' => 'Új felhasználó hozzáadása',
-	'recover_password' => 'Jelszó emlékeztető',
-	'users' => 'Felhasználók',
-	'create_user' => 'Új felhasználó készítése',
-	'reset_password' => 'Jelszó törlése',
-	'reset_password_for' => 'Jelszó törlése ',
-	'reset_password_explain' => 'Kérlek olyan jelszót adj meg, amit nem felejtesz el.',
-	'back_to' => 'Vissza ',
+	'users' => 'Felhasználó',
 
-	// english spelling confusion
-	'log_in' => 'Belépni', // verb
-	'login' => 'Belépés', // noun
+	'create_user' => 'Új felhasználó',
+	'add_user' => 'Új felhasználó hozzáadás',
+	'editing_user' => '%s adatlap szerkesztés',
+	'remembered' => 'Megvan a jelszavam',
+	'forgotten_password' => 'Új jelszó kérése',
 
-	'forgotten_password' => 'Elfelejtetted a jelszavad?',
+	// roles
+	'administrator' => 'Admin',
+	'administrator_explain' => '',
 
-	'recover' => 'Visszaállítás',
-	'editing' => 'Szerkesztés',
-	'profile' => 'profil',
-
-	'real_name' => 'Valós név',
-	'real_name_explain' => 'A felhasználó valódi neve. A szerzőnél látszik.',
-	'bio' => 'Bemutatkozás',
-	'bio_explain' => 'Rövid leírás a felhasználóról. Érvényes HTML kóddal.',
-	'status' => 'Státusz',
-	'inactive' => 'Inaktív',
-	'active' => 'Aktív',
-	'status_explain' => 'Ha inaktív, a felhasználó nem tud bejelentkezni.',
-	'role' => 'Jogosultság',
-	'administrator' => 'Adminisztrátor',
 	'editor' => 'Szerkesztő',
+	'editor_explain' => '',
+
 	'user' => 'Felhasználó',
-	'role_explain' => 'A felhasználó jogosultsága. Bővebb infó <a href="//anchorcms.com/docs/roles">itt</a>.',
-	'user_details' => 'Felhasználói adatok',
-	'user_details_explain' => 'Adj meg több részletet az új felhasználóról, aki bejelentkezik az Anchor-be.',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => 'Teljes név',
+	'real_name_explain' => '',
+
+	'bio' => 'Bemutatkozás',
+	'bio_explain' => '',
+
+	'status' => 'Állapot',
+	'status_explain' => '',
+
+	'role' => 'Szerepkör',
+	'role_explain' => '',
+
 	'username' => 'Felhasználónév',
-	'username_explain' => 'A kívánt felhasználónév. Később megváltoztatható.',
+	'username_explain' => '',
+	'username_missing' => 'Adjuk meg a felhasználónevet, amely legalább %s karaktert tartalmazhat.',
+
 	'password' => 'Jelszó',
-	'password_explain' => 'És a megfelelő jelszó. Később megváltoztatható.',
-	'password_blank' => 'Hagyd üresen, ha nem változik.',
-	'email' => 'Email',
-	'email_explain' => 'A felhasználó email címe. Szükséges, ha a felhasználó elfelejti a jelszavát.',
+	'password_explain' => '',
+	'password_too_short' => 'A jelszó legalább %s karaktert tartalmazhat.',
 
-	'create' => 'Létrehoz',
-	'update' => 'Frissít',
-	'delete' => 'Töröl',
-	'submit' => 'Küld',
-	'return_users' => 'Vissza a felhasználókhoz',
-	
-	'delete_confirm' => 'Biztos hogy szeretnéd törölni ezt a felhasználót?', 
-	'delete_confirm_submit' => 'Igen, tisztában vagyok a végleges törléssel kapcsolatban', 
-	'delete_confirm_cancel' => 'Ne, tartsuk meg ezt a felhasználót',
+	'new_password' => 'Új jelszó',
 
-	'username_exists' => 'A felhasználónév már használatban van',
-	'missing_password' => 'Kérlek add meg a jelszót',
-	'password_too_short' => 'A jelszavad legalább %s karakter hosszú legyen',
-	'invalid_email' => 'Kérlek valós email címet adj meg',
-	'missing_name' => 'Kérlek add meg a megjelenített nevet',
-	'missing_username' => 'Kérlek add me ga felhasználónevet',
-	'invalid_status' => 'Nem tudod a saját állapotodat megváltoztatni',
-	'user_success_created' => 'Új felhasználó hozzáadva',
-	'user_success_updated' => 'A felhasználó frissítve',
-	'user_success_deleted' => 'A felhasználó törölve',
-	'user_success_password' => 'Az új jelszavad elküldve',
-	'user_notice_recover' => 'Küldtünk egy levelet, hogy megváltozott a jelszavad.',
-	'user_email_recover' => 'Kérdet, állítsuk vissza a jelszavadat. A folytatáshoz kövesd a linket.' . PHP_EOL . '%s',
-	'user_subject_recover' => 'Jelszó törlése',
-	'invalid_account' => 'A fiók nem található',
-	'missing_login_username' => 'Kérlek add meg a felhasználóneved',
-	'missing_login_password' => 'Kérlek add meg a jelszavad'
+	'email' => 'E-mail',
+	'email_explain' => '',
+	'email_missing' => 'Érvényes e-mail címet adjunk meg.',
+	'email_not_found' => 'Az adatlap nem található.',
+
+	// messages
+	'updated' => 'Felhasználói adatlap frissítve.',
+	'created' => 'Felhasználói adatlap létrehozva.',
+	'deleted' => 'Felhasználói adatlap törölve.',
+	'delete_error' => 'A saját adatlapot nem lehet törölni.',
+	'login_error' => 'Érvénytelen felhasználónév vagy jelszó.',
+	'logout_notice' => 'A kijelentkezés megtörtént.',
+	'recovery_sent' => 'A jelszó visszaállítási kérelem elküldésre került a megadott e-mail címre.',
+	'recovery_expired' => 'A jelszó visszaállítási kulcs lejárt. Próbáljuk meg újra.',
+	'password_reset' => 'Az új jelszó beállításra került. Jelentkezzünk be újra.',
+
+	// password recovery email
+	'recovery_subject' => 'Jelszó visszaállítás',
+	'recovery_message' => 'Jelszó visszaállítási kérelem érkezett a rendszerhez.' .
+		'A következő hivatkozáson a jelszó megváltoztatható.' . PHP_EOL . '%s',
 
 );
