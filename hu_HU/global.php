1d40c3accf9fdf1fe56fdb754aacc2bb0442f1c4
Laszlo Espadas <kw@kardiweb.org>
2014-05-08 21:25:00 +0200

    Create hungarian translation 0.9.2

---
+++ hu_HU/global.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Mentés',
+	'delete' => 'Törlés',
+	'update' => 'Frissítés',
+	'edit' => 'Szerkesztés',
+	'editing' => 'Szerkesztés',
+	'create' => 'Létrehozás',
+	'created' => 'Létrehozva',
+	'submit' => 'Beküldés',
+	'close' => 'Bezárás',
+	'status' => 'Állapot',
+	'manage' => 'Adminisztráció',
+	'reset' => 'Visszaállítás',
+	'all' => 'Összes',
+
+	// pagination
+	'next' => 'Következő',
+	'previous' => 'Előző',
+	'first' => 'Előző',
+	'last' => 'Következő',
+
+	// statuses
+	'draft' => 'Vázlat',
+	'archived' => 'Archiválva',
+	'published' => 'Közzétéve',
+	'pending' => 'Függőben',
+	'approved' => 'Elfogadva',
+	'spam' => 'Spam',
+
+	'inactive' => 'Nem elérhető',
+	'active' => 'Elérhető',
+
+	// roles
+	'administrator' => 'Admin',
+	'editor' => 'Szerző',
+	'user' => 'Felhasználó',
+
+	'log_in' => 'Belépés',
+	'login' => 'Belépés',
+	'log_out' => 'Kilépés',
+	'logout' => 'Kilépés',
+
+	// pharses
+	'visit_your_site' => 'Előnézet',
+	'powered_by_anchor' => 'Anchor cms %s | <a target="_blank" href="http://www.kardiweb.org/">Magyar fordítás</a>',
+	'make_blogging_beautiful' => 'Személyes publikációs platform',
+
+	// intro
+	'welcome_to_anchor' => 'Üdvözlet',
+	'welcome_to_anchor_lets_go' => 'Üdvözlet! Kezdjük el.',
+	'run_the_installer' => 'Telepítő indítása',
+
+	// upgrade
+	'upgrade' => 'Frissítés',
+	'good_news' => 'Jó hír!',
+	'new_version_available' => 'Új verzió érhető el.',
+	'download_now' => 'Letöltés',
+	'upgrade_later' => 'Frissítés késöbb',
+
+	// debug profiler
+	'profile' => 'Adatlap',
+	'profile_memory_usage' => 'Felhasznált memória',
+
+	// messages
+	'confirm_delete' => 'Biztosak vagyunk benne, hogy töröljük? Ez a művelet nem vonható vissza!'
+
+);
