ed3e7a2462ee57c9934897f49d8f900048de2326
Nikolay <nikolay.kim@outlook.com>
2014-09-08 14:40:04 +0400

    Update global.php
    Translations for recently added strings

--- ru_RU/global.php
+++ ru_RU/global.php
@@ -40,15 +40,36 @@ return array(
 	'inactive' => 'Неактивный',
 	'active' => 'Активный',
 
+	// roles
+	'administrator' => 'Администратор',
+	'editor' => 'Редактор',
+	'user' => 'Пользователь',
+
 	'log_in' => 'Вход',
 	'login' => 'Войти',
 	'log_out' => 'Выход',
 	'logout' => 'Выйти',
 
 	// pharses
-	'visit_your_site' => 'Сайт',
+	'visit_your_site' => 'Перейти на сайт',
 	'powered_by_anchor' => 'Работает на Anchor, версии %s',
 	'make_blogging_beautiful' => 'Делает мир лучше.',
+
+	// intro
+	'welcome_to_anchor' => 'Добро пожаловать',
+	'welcome_to_anchor_lets_go' => 'Спасибо, что выбрали Anchor. Приступим!',
+	'run_the_installer' => 'Установить',
+
+	// upgrade
+	'upgrade' => 'Проверка обновлений',
+	'good_news' => 'Отличные новости!',
+	'new_version_available' => 'Появилась новая версия Anchor.',
+	'download_now' => 'Установить сейчас',
+	'upgrade_later' => 'Отложить на потом',
+
+	// debug profiler
+	'profile' => 'Профилировка',
+	'profile_memory_usage' => 'Использование памяти',
 
 	// messages
 	'confirm_delete' => 'Вы действительно хотите это удалить? '
