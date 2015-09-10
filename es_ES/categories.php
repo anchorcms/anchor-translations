73b8141e550c2d9fc91f875c5093ee10dc7b44df
Chrystopher.Medina <chrystopher.medina@dfsoft.com.mx>
2015-09-10 04:22:59 -0500

    add more traslation files for es_ES

---
+++ es_ES/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+    'category' => 'Categor&iacute;a',
+    'categories' => 'Categorias',
+
+    'create_category' => 'Crear nueva Categor&iacute;a',
+    'edit_category' => 'Editar Categor&iacute;a',
+
+    // form fields
+    'title' => 'T&iacute;tulo',
+    'title_explain' => 'T&iacute;tulo de tu categor&iacute;a.',
+    'title_missing' => 'Ingresa un t&iacute;tulo',
+
+    'slug' => 'Slug',
+    'slug_explain' => 'Slug para tu categor&iacute;a.',
+
+    'description' => 'Descripci&oacute;n',
+    'description_explain' => 'De que trata tu categor&iacute;a',
+
+    // messages
+    'created' => 'Categoría agregada.',
+    'updated' => 'Categoría editada.',
+    'deleted' => 'Categoría eliminada.',
+    'delete_error' => 'Debes tener al menos una categor&iacute;a.',
+
+);
