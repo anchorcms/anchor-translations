00a8ad3407fa6eff7d18c2b24e3fa2246b7f4e21
Alexandr <alexfox@protonmail.ch>
2015-12-24 02:59:42 +0500

    Updated users.php

--- ru_RU/users.php
+++ ru_RU/users.php
@@ -17,11 +17,11 @@ return array(
 	'editor' => 'Редактор',
 	'editor_explain' => '',
 
-	'user' => 'Подписчик',
+	'user' => 'Читатель',
 	'user_explain' => '',
 
 	// form fields
-	'real_name' => 'Реальное имя',
+	'real_name' => 'Имя',
 	'real_name_explain' => '',
 
 	'bio' => 'О себе',
@@ -35,7 +35,7 @@ return array(
 
 	'username' => 'Логин',
 	'username_explain' => '',
-	'username_missing' => 'Пожалуйста введите логин. Может содержаться до %s букв.',
+	'username_missing' => 'Пожалуйста, введите логин. Он может содержать до %s букв.',
 
 	'password' => 'Пароль',
 	'password_explain' => '',
@@ -49,14 +49,14 @@ return array(
 	'email_not_found' => 'Пользователь не найден.',
 
 	// messages
-	'updated' => 'Профиль пользователя обновлен.',
-	'created' => 'Профиль пользователя добавлен.',
-	'deleted' => 'Профиль пользователя удален.',
+	'updated' => 'Профиль пользователя обновлен',
+	'created' => 'Пользователь успешно создан',
+	'deleted' => 'Пользователь удален',
 	'delete_error' => 'Вы не можете удалить свой профиль.',
 	'login_error' => 'Неверный логин или пароль.',
-	'logout_notice' => 'Вы вышли.',
-	'recovery_sent' => 'Пожалуйста, проверьте свой почтовый ящик.',
-	'recovery_expired' => 'Время для сброса пароля истекло. Попробуйте еще раз.',
+	'logout_notice' => 'Вы вышли',
+	'recovery_sent' => 'Пожалуйста, проверьте свой почтовый ящик',
+	'recovery_expired' => 'Время для сброса пароля истекло. Попробуйте еще раз',
 	'password_reset' => 'Ваш пароль изменен. Вперед!',
 
 	// password recovery email
