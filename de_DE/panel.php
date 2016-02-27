5910c68fdd0d7677d64894567d7895a7ba8ceb13
lschuermann <lschuermann@users.noreply.github.com>
2016-02-27 10:47:56 +0100

    Add panel-translation.

---
+++ de_DE/panel.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+        'panel' => 'Administrations-Bereich',
+        'title' => 'Willkommen zu deiner Anchor-Seite',
+        'message' => 'Hier wirst du alle Werkzeuge finden, die du brauchst um Inhalte f&uuml;r deine Website zu erstellen. Des Weiteren kannst du hier Nutzer, Eintr&auml;ge und Seiten verwalten. Um mehr dar&uuml;ber zu erfahren, was du mit Anchor machen kannst, besuche bitte unsere Dokumentation: <a href="https://anchorcms.com/docs">http://anchorcms.com/docs</a> (Englisch)',
+
+);
