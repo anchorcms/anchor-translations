508924aaecd268ffb83f21e2c71a65b9e896db87
IronPike <zhangtesso@gmail.com>
2016-06-05 15:45:56 +0800

    Add panel.php

---
+++ zh_CN/panel.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+    'panel' => '管理界面',
+    'title' => '欢迎来到您自己的 Anchor 站点',
+    'message' => '在这里，您能找到您需要的所有工具：为站点添加内容，管理用户、文章以及页面。想要了解Anchor的更多功能，请查阅我们的文档：<a href="https://anchorcms.com/docs">http://anchorcms.com/docs</a>',
+    
+);
