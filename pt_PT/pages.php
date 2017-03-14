f53678788c199fa82a1d653fa2335bc2d20f377f
Ricardo Cino <ricardocino@Ricardos-MacBook-Pro.local>
2017-03-14 20:39:35 +0100

    Autoformatting the pt_PT files

--- pt_PT/pages.php
+++ pt_PT/pages.php
@@ -1,45 +1,45 @@
 <?php
 
-return array(
+return [
 
-	'pages' => 'Páginas',
+    'pages' => 'Páginas',
 
-	'create_page' => 'Criar uma nova página',
-	'nopages_desc' => 'Não existem páginas.',
-	'redirect' => 'Redireccionar',
+    'create_page' => 'Criar uma nova página',
+    'nopages_desc' => 'Não existem páginas.',
+    'redirect' => 'Redireccionar',
 
-	// form fields
-	'redirect_url' => 'Url de Redireccionamento',
-	'redirect_missing' => 'Insira um URL válido',
+    // form fields
+    'redirect_url' => 'Url de Redireccionamento',
+    'redirect_missing' => 'Insira um URL válido',
 
-	'title' => 'Título da página',
-	'title_explain' => '',
-	'title_missing' => 'Insira um título da página',
+    'title' => 'Título da página',
+    'title_explain' => '',
+    'title_missing' => 'Insira um título da página',
 
-	'content' => 'Conteúdo',
-	'content_explain' => 'Aqui fica o conteúdo da página. Usa Markdown.',
+    'content' => 'Conteúdo',
+    'content_explain' => 'Aqui fica o conteúdo da página. Usa Markdown.',
 
-	'show_in_menu' => 'Visível no Menu',
-	'show_in_menu_explain' => '',
+    'show_in_menu' => 'Visível no Menu',
+    'show_in_menu_explain' => '',
 
-	'name' => 'Nome',
-	'name_explain' => '',
+    'name' => 'Nome',
+    'name_explain' => '',
 
-	'slug' => 'URL',
-	'slug_explain' => 'Coloque um URL de identificação, os URLs contêm apenas caracteres alfanuméricos, sem acentos nem espaços.',
-	'slug_missing' => 'Coloque um URL, os URLs contêm apenas caracteres alfanuméricos, sem acentos nem espaços.',
-	'slug_duplicate' => 'Este URL já existe',
-	'slug_invalid' => 'O URL tem de conter letras',
+    'slug' => 'URL',
+    'slug_explain' => 'Coloque um URL de identificação, os URLs contêm apenas caracteres alfanuméricos, sem acentos nem espaços.',
+    'slug_missing' => 'Coloque um URL, os URLs contêm apenas caracteres alfanuméricos, sem acentos nem espaços.',
+    'slug_duplicate' => 'Este URL já existe',
+    'slug_invalid' => 'O URL tem de conter letras',
 
-	'status' => 'Estado',
-	'status_explain' => '',
+    'status' => 'Estado',
+    'status_explain' => '',
 
-	'parent' => 'Página acima desta',
-	'parent_explain' => '',
+    'parent' => 'Página acima desta',
+    'parent_explain' => '',
 
-	// messages
-	'updated' => 'A página foi actualizada.',
-	'created' => 'A página foi criada.',
-	'deleted' => 'A página foi eliminada.'
+    // messages
+    'updated' => 'A página foi actualizada.',
+    'created' => 'A página foi criada.',
+    'deleted' => 'A página foi eliminada.'
 
-);
+];
--- pt_PT/posts.php
+++ pt_PT/posts.php
@@ -1,47 +1,47 @@
 <?php
 
-return array(
+return [
 
-	'posts' => 'Artigos',
+    'posts' => 'Artigos',
 
-	'create_post' => 'Criar um novo artigo',
-	'noposts_desc' => 'Você não tem nenhum artigo!',
+    'create_post' => 'Criar um novo artigo',
+    'noposts_desc' => 'Você não tem nenhum artigo!',
 
-	// form fields
-	'title' => 'Título do artigo',
-	'title_explain' => '',
-	'title_missing' => 'Insira um título',
+    // form fields
+    'title' => 'Título do artigo',
+    'title_explain' => '',
+    'title_missing' => 'Insira um título',
 
-	'content' => 'Conteúdo do artigo',
-	'content_explain' => 'Basta escrever.',
+    'content' => 'Conteúdo do artigo',
+    'content_explain' => 'Basta escrever.',
 
-	'slug' => 'URL',
-	'slug_explain' => 'Coloque um URL de identificação, os URLs contêm apenas caracteres alfanuméricos, sem acentos nem espaços.',
-	'slug_missing' => 'Coloque um URL, os URLs contêm apenas caracteres alfanuméricos, sem acentos nem espaços.',
-	'slug_duplicate' => 'Este URL já existe',
-	'slug_invalid' => 'O URL tem de conter letras',
+    'slug' => 'URL',
+    'slug_explain' => 'Coloque um URL de identificação, os URLs contêm apenas caracteres alfanuméricos, sem acentos nem espaços.',
+    'slug_missing' => 'Coloque um URL, os URLs contêm apenas caracteres alfanuméricos, sem acentos nem espaços.',
+    'slug_duplicate' => 'Este URL já existe',
+    'slug_invalid' => 'O URL tem de conter letras',
 
-	'description' => 'Descrição',
-	'description_explain' => '',
+    'description' => 'Descrição',
+    'description_explain' => '',
 
-	'status' => 'Estado',
-	'status_explain' => '',
+    'status' => 'Estado',
+    'status_explain' => '',
 
-	'category' => 'Categoria',
-	'category_explain' => '',
+    'category' => 'Categoria',
+    'category_explain' => '',
 
-	'allow_comments' => 'Permitir Comentários',
-	'allow_comments_explain' => '',
+    'allow_comments' => 'Permitir Comentários',
+    'allow_comments_explain' => '',
 
-	'custom_css' => 'CSS personalizado',
-	'custom_css_explain' => '',
+    'custom_css' => 'CSS personalizado',
+    'custom_css_explain' => '',
 
-	'custom_js' => 'JS personalizado',
-	'custom_js_explain' => '',
+    'custom_js' => 'JS personalizado',
+    'custom_js_explain' => '',
 
-	// messages
-	'updated' => 'O seu artigo foi actualizado',
-	'created' => 'O seu novo artigo foi criado',
-	'deleted' => 'O seu artigo foi eliminado'
+    // messages
+    'updated' => 'O seu artigo foi actualizado',
+    'created' => 'O seu novo artigo foi criado',
+    'deleted' => 'O seu artigo foi eliminado'
 
-);
+];
