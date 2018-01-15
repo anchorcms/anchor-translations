308edbb71ef03796a82cd63f61cdb6d06aa0c8f4
artlancer <35227811+artlancer@users.noreply.github.com>
2018-01-15 21:57:05 +0500

    Update posts.php
    Actual translation

--- ru_RU/posts.php
+++ ru_RU/posts.php
@@ -2,50 +2,50 @@
 
 return array(
 
-	'posts' => 'Записи',
+    'posts' => 'Посты',
 
-	'create_post' => 'Создать запись',
-	'noposts_desc' => 'Здесь пусто',
+    'create_post' => 'Создать пост',
+    'noposts_desc' => 'У Вас нет ни одного поста!',
 
-	// form fields
-	'title' => 'Заголовок',
-	'title_explain' => '',
-	'title_missing' => 'Пожалуйста, введите заголовок',
+    // form fields
+    'title' => 'Заголовок поста',
+    'title_explain' => '',
+    'title_missing' => 'Заголовок должен содержать минимум 3 символа',
 
-	'content' => 'Содержимое записи',
-	'content_explain' => 'Ваша фантазия',
+    'content' => 'Контент',
+    'content_explain' => 'Просто пишите.',
 
-	'slug' => 'Адрес',
-	'slug_explain' => 'Уникальная ссылка на Вашу запись',
-	'slug_missing' => 'Пожалуйста, введите адрес',
-	'slug_duplicate' => 'Такой адрес уже используется',
-	'slug_invalid' => 'Адрес должен содержать буквы',
+    'slug' => 'ЧПУ и ID(Slug)',
+    'slug_explain' => 'Slug uri нужен для идентификации Вашго поста, может содержать только ascii символы',
+    'slug_missing' => 'Slug должен содержать минимум 3 символа, используйте только ascii символы',
+    'slug_duplicate' => 'Такой Slug уже существует',
+    'slug_invalid' => 'Slug должен содержать буквы',
 
-	'time' => 'Опубликовано (GMT)',
-	'time_explain' => 'Шаблон: YYYY-MM-DD HH:MM:SS',
-	'time_invalid' => 'Ошибка в шаблоне даты-времени',
+    'time' => 'Опубликовано (GMT)',
+    'time_explain' => 'Шаблон: YYYY-MM-DD HH:MM:SS',
+    'time_invalid' => 'Недопустимый шаблон времени',
 
-	'description' => 'Описание',
-	'description_explain' => '',
+    'description' => 'Описание',
+    'description_explain' => '',
 
-	'status' => 'Статус',
-	'status_explain' => '',
+    'status' => 'Статус',
+    'status_explain' => '',
 
-	'category' => 'Категория',
-	'category_explain' => '',
+    'category' => 'Категория',
+    'category_explain' => '',
 
-	'allow_comments' => 'Разрешить комментирование?',
-	'allow_comments_explain' => '',
+    'allow_comments' => 'Разрешить комментарии',
+    'allow_comments_explain' => '',
 
-	'custom_css' => 'CSS',
-	'custom_css_explain' => '',
+    'custom_css' => 'Свой CSS',
+    'custom_css_explain' => '',
 
-	'custom_js' => 'JS',
-	'custom_js_explain' => '',
+    'custom_js' => 'Свой JS',
+    'custom_js_explain' => '',
 
-	// messages
-	'updated' => 'Запись обновлена',
-	'created' => 'Запись была добавлена',
-	'deleted' => 'Запись удалена'
+    // messages
+    'updated' => 'Пост обновлен',
+    'created' => 'Пост создан',
+    'deleted' => 'Пост удален'
 
 );
