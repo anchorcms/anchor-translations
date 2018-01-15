a44530a3dc2a64675fb13c3324505505607e083e
artlancer <35227811+artlancer@users.noreply.github.com>
2018-01-15 21:53:34 +0500

    Update metadata.php
    Actual translation

--- ru_RU/metadata.php
+++ ru_RU/metadata.php
@@ -2,43 +2,49 @@
 
 return array(
 
-	'metadata' => 'Настройки',
-	'metadata_desc' => 'Управление вашим сайтом',
+    'metadata' => 'Настройки сайта',
+    'metadata_desc' => 'Настройте сайт',
 
-	'comment_settings' => 'Комментарии',
-	'theme_settings' => 'Внешний вид',
+    'comment_settings' => 'Комментарии',
+    'theme_settings' => 'Внежний вид(шаблон)',
 
-	// form fields
-	'sitename' => 'Название',
-	'sitename_explain' => '',
-	'sitename_missing' => 'Вашему сайту необходимо имя!',
+    // form fields
+    'sitename' => 'Название сайта',
+    'sitename_explain' => '',
+    'sitename_missing' => 'Вашему сайту нужно Название!',
 
-	'sitedescription' => 'Описание',
-	'sitedescription_explain' => '',
-	'sitedescription_missing' => 'Вашему сайту необходимо описание!',
+    'sitedescription' => 'Описание сайта',
+    'sitedescription_explain' => 'Описание Вашего ссайта',
+    'sitedescription_missing' => 'Вашему сайту нужно Описание!',
 
-	'homepage' => 'Главная страница',
-	'homepage_explain' => '',
+    'homepage' => 'Главная страница',
+    'homepage_explain' => '',
 
-	'postspage' => 'Страница с записями',
-	'postspage_explain' => '',
+    'postspage' => 'Страница с постами',
+    'postspage_explain' => '',
 
-	'posts_per_page' => 'Записей на странице',
-	'posts_per_page_explain' => '',
+    'dashboard_page' => 'Страница Админ Панели',
+    'dashboard_page_explain' => 'Страница Админ Панели по умолчанию',
 
-	'auto_publish_comments' => 'Автоматически публиковать комментарии?',
-	'auto_publish_comments_explain' => '',
+    'posts_per_page' => 'Постов на странице',
+    'posts_per_page_explain' => '',
 
-	'comment_notifications' => 'Отправлять уведомления о новых комментариях?',
-	'comment_notifications_explain' => '',
+    'show_all_posts' => 'Показать все посты',
+    'show_all_posts_explain' => 'Показывать посты без пагинации (может работать медленно)',
 
-	'comment_moderation_keys' => 'Стоп-слова',
-	'comment_moderation_keys_explain' => 'Разделяйте запятыми. Такие комментарии будут помечены как спам.',
+    'auto_publish_comments' => 'Автоматическая публикация комментариев',
+    'auto_publish_comments_explain' => '',
 
-	'current_theme' => 'Шаблон',
-	'current_theme_explain' => '',
+    'comment_notifications' => 'Уведомление по email о новых комментариях',
+    'comment_notifications_explain' => '',
 
-	// messages
-	'updated' => 'Настройки сохранены',
+    'comment_moderation_keys' => 'Ключевые слова для спам-фильтра',
+    'comment_moderation_keys_explain' => 'Перечислите ключевые слова для спам фильтра через запятую. Комментарии, содержащие эти слова автоматически будут отправлены в спам.',
+
+    'current_theme' => 'Текущая тема',
+    'current_theme_explain' => '',
+
+    // messages
+    'updated' => 'Метаданные обновлены',
 
 );
